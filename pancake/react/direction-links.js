9052ba4c7eef67e9ac2dac054db0f4608f4c5fd0
Chris Lee <sirleech@gmail.com>
2018-05-14 08:40:07 +1000

    first

---
+++ pancake/react/direction-links.js
@@ -0,0 +1,69 @@
+/*! @gov.au/direction-links v1.0.1 */
+/***************************************************************************************************************************************************************
+ *
+ * direction-links function
+ *
+ * Use direction links to link up and down within the DOM or back to a parent page.
+ *
+ **************************************************************************************************************************************************************/
+
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+
+// The following line will be replaced automatically with generic imports for the ES5 pipeline.
+// You can safely ignore this bit if you use this module with pancake
+//
+// [replace-imports]
+
+
+/**
+ * All direction options
+ *
+ * @type {Object}
+ */
+const directions = {
+	up: 'au-direction-link--up',
+	right: 'au-direction-link--right',
+	down: 'au-direction-link--down',
+	left: 'au-direction-link--left',
+};
+
+
+/**
+ * DEFAULT
+ * The direction-links component
+ *
+ * @param  {string} dark             - Add the dark variation class
+ * @param  {string} link             - The link target, optional
+ * @param  {string} text             - The text of the CTA link
+ * @param  {string} direction        - The direction for the arrow; can be either: up right down left, default: 'right'
+ * @param  {string} className        - An additional class, optional
+ * @param  {object} attributeOptions - Any other attribute options
+ */
+const AUdirectionLink = ({ dark, link, text, direction, className = '', ...attributeOptions }) => {
+	if( link ) {
+		return (
+			<a className={ `au-direction-link ${ className } ${ directions[ direction ] }${ dark ? ' au-direction-link--dark' : '' }` } href={ link } { ...attributeOptions }>{ text }</a>
+		);
+	}
+	else {
+		return (
+			<button className={ `au-direction-link ${ className } ${ directions[ direction ] }${ dark ? ' au-direction-link--dark' : '' }` } { ...attributeOptions }>{ text }</button>
+		);
+	}
+};
+
+AUdirectionLink.propTypes = {
+	dark: PropTypes.bool,
+	link: PropTypes.string,
+	text: PropTypes.string.isRequired,
+	direction: PropTypes.oneOf([ 'up', 'right', 'down', 'left' ]).isRequired,
+	className: PropTypes.string,
+};
+
+AUdirectionLink.defaultProps = {
+	direction: 'right',
+};
+
+export default AUdirectionLink;
---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import AUdirectionLink from '@gov.au/direction-links';
+import AUaccordion from '@gov.au/accordion';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Hello Ui Kit!</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h2>Direction links</h2>
+        <AUdirectionLink link="#url" text="Back" direction="left" />
+        <AUdirectionLink link="#url" text="Next" />
+        <AUdirectionLink link="#url" text="Top" direction="up" />
+        <AUdirectionLink link="#url" text="Skip to footer" direction="down" />
+
+        <h2>Accordion</h2>
+        <AUaccordion speed={0} header="Accordion title">
+          Here <a href="#url">is</a> some accordion content
+        </AUaccordion>
+
+      </div>
+    );
+  }
+}
+
+export default App;
