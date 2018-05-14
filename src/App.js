7a70e30221e7173bcfef84dfda298a27c8367eb3
Chris Lee <sirleech@gmail.com>
2018-05-14 11:33:26 +1000

    add class=js in HTML tag to get accordion to work

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
         <AUdirectionLink link="#url" text="Skip to footer" direction="down" />
 
         <h2>Accordion</h2>
-        <AUaccordion header="Accordion title">
+        <AUaccordion open header="Accordion title">
           Here <a href="#url">is</a> some accordion content
         </AUaccordion>
 
